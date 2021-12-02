6b866603dfdd59ee7c4d4313673cee778d70a7d9
Sid Parikh <parikhsid16@gmail.com>
2021-12-01 19:32:30 -0500

    Create Javadoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.sidparikh.advent"},{"l":"com.sidparikh.advent.solutions"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.sidparikh.advent.solutions","l":"Day01"},{"p":"com.sidparikh.advent","l":"Solution"},{"p":"com.sidparikh.advent","l":"TestSolutions"}];updateSearchResults();
