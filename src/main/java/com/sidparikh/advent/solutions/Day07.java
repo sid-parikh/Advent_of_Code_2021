e1e0fe00331436ba62feacf2a93c2509fabdaaa9
Sid Parikh <parikhsid16@gmail.com>
2021-12-07 13:21:27 -0500

    Day 7 Pretty

--- src/main/java/com/sidparikh/advent/solutions/Day07.java
+++ src/main/java/com/sidparikh/advent/solutions/Day07.java
@@ -4,22 +4,52 @@ import com.sidparikh.advent.Solution;
 
 import java.util.Arrays;
 
+/**
+ * Day 7: The Treachery of Whales
+ * <p>
+ * The horizontally moving crabs in submarines are going to save us from the scary whale. Let's help them line up! <br>
+ * Star One: Using a linear model for fuel cost vs. distance, find the optimal place for the ships to go to. <br>
+ * Star Two: The calculation for fuel cost for n units is actually (1 + 2 + ... + n).
+ */
 public class Day07 extends Solution {
     public Day07() {
         super(7);
     }
 
+    /**
+     * Calculates fuel cost based on the Star Two formula: where the cost to move a ship n units is
+     * (1 + 2 + ... + n) = n(n + 1)/2
+     *
+     * @param nums   a list of initial positions of ships
+     * @param target the position to move all the ships to
+     * @return the resultant fuel cost
+     */
+    private static long calculateFuelTwo(int[] nums, int target) {
+
+        long sum = 0;
+        for (int n : nums) {
+            int diff = Math.abs(n - target);
+            sum += (long) diff * (diff + 1) / 2;
+        }
+
+        return sum;
+    }
+
+    /**
+     * Calculates the fuel cost to move all the ships to the median of their positions.
+     *
+     * @return the fuel cost (342641)
+     */
     @Override
     public String partOne() {
         int[] nums = Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).toArray();
 
-        // I think the target is the median
-
+        // Target is the median.
         Arrays.sort(nums);
 
         int target;
         if (nums.length % 2 == 0) {
-            target = (nums[nums.length / 2] + nums[nums.length / 2 - 1])/2;
+            target = (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2;
         } else {
             target = nums[nums.length / 2];
         }
@@ -30,6 +60,12 @@ public class Day07 extends Solution {
 
     }
 
+    /**
+     * Calculates the lowest possible fuel cost if all ships are moved to any single point and the fuel cost to move a
+     * ship n units is calculated by {@link #calculateFuelTwo(int[], int)}.
+     *
+     * @return the optimized fuel cost (93006301)
+     */
     @Override
     public String partTwo() {
         int[] nums = Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).toArray();
@@ -58,16 +94,5 @@ public class Day07 extends Solution {
 
         return String.valueOf(smallestSum);
         // Fun fact it's not too inefficient to work!
-    }
-
-    private static long calculateFuelTwo(int[] nums, int target) {
-
-        long sum = 0;
-        for (int n : nums) {
-            int diff = Math.abs(n - target);
-            sum += (long) diff * (diff + 1) / 2;
-        }
-
-        return sum;
     }
 }
