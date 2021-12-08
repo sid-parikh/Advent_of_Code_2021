10498f06006bef5dfbaaae632fedd248de920ab9
Sid Parikh <parikhsid16@gmail.com>
2021-12-08 11:10:08 -0500

    Da 8 ugly

--- src/main/java/com/sidparikh/advent/solutions/Day08.java
+++ src/main/java/com/sidparikh/advent/solutions/Day08.java
@@ -3,27 +3,64 @@ package com.sidparikh.advent.solutions;
 import com.sidparikh.advent.Solution;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day08 extends Solution {
+    private final List<NotebookEntry> entryList = new ArrayList<>();
+
     public Day08() {
         super(8);
     }
 
-    private record Entry(String[] signalPatterns, String[] outputValue) {}
+    private static int evaluateDigit(String digit, Map<Character, Character> key) {
 
-    private final List<Entry> entryList = new ArrayList<>();
+        final int l = digit.length();
+
+        if (l == 2) return 1;
+        if (l == 4) return 4;
+        if (l == 3) return 7;
+        if (l == 7) return 8;
+
+        if (l == 5) {
+
+            if (digit.contains(String.valueOf(key.get('g')))) {
+                final boolean c = digit.contains(String.valueOf(key.get('c')));
+                final boolean f = digit.contains(String.valueOf(key.get('f')));
+                if (c && f) return 3;
+                if (c) return 2;
+                if (f) return 5;
+            }
+
+        }
+
+        if (l == 6) {
+
+            if (!digit.contains(String.valueOf(key.get('e')))) {
+                return 9;
+            } else if (digit.contains(String.valueOf(key.get('d')))){
+                return 6;
+            } else {
+                return 0;
+            }
+
+        }
+
+        return -1;
+
+    }
 
     @Override
     public String partOne() {
         for (String s : input) {
             String[] line = s.split("\\|");
-            Entry e = new Entry(line[0].split(" "), line[1].split(" "));
+            NotebookEntry e = new NotebookEntry(line[0].trim().split(" "), line[1].trim().split(" "));
             entryList.add(e);
         }
 
         int result = 0;
-        for (Entry e : entryList) {
+        for (NotebookEntry e : entryList) {
             for (String digit : e.outputValue()) {
                 final int l = digit.length();
                 if (l == 2 || l == 3 || l == 4 || l == 7) {
@@ -38,34 +75,101 @@ public class Day08 extends Solution {
     @Override
     public String partTwo() {
 
-        for (Entry e : entryList) {
-            String[] numberRepresentations = new String[10];
+        int answer = 0;
+        for (NotebookEntry e : entryList) {
 
-            // Ordered a to g in the original format
-            String[] decodedPositions = new String[6];
+            HashMap<Character, Integer> letterFrequencies = new HashMap<>();
+            letterFrequencies.put('a', 0);
+            letterFrequencies.put('b', 0);
+            letterFrequencies.put('c', 0);
+            letterFrequencies.put('d', 0);
+            letterFrequencies.put('e', 0);
+            letterFrequencies.put('f', 0);
+            letterFrequencies.put('g', 0);
 
-            for (String digit : e.signalPatterns()) {
-                final int l = digit.length();
+            String one = "";
+            String four = "";
+            String seven = "";
 
-                if (l == 2) {
-                    numberRepresentations[1] = digit;
-                } else if (l == 3) {
-                    numberRepresentations[7] = digit;
-                } else if (l == 4) {
-                    numberRepresentations[4] = digit;
+            HashMap<Character, Character> key = new HashMap<>();
+
+            for (String s : e.signalPatterns()) {
+
+                if (s.length() == 2) {
+                    one = s;
+                } else if (s.length() == 4) {
+                    four = s;
+                } else if (s.length() == 3) {
+                    seven = s;
+                }
+
+                for (char c : s.toCharArray()) {
+                    letterFrequencies.put(c, letterFrequencies.get(c) + 1);
                 }
 
             }
 
-            for (int i = 0; i < numberRepresentations[7].length(); i++) {
-                if (!numberRepresentations[1].contains(numberRepresentations[7].substring(i, i+1))) {
-                    decodedPositions[0] = numberRepresentations[7].substring(i, i + 1);
+            // System.out.println(letterFrequencies);
+
+
+            for (int i = 0; i < seven.length(); i++) {
+                if (!one.contains(seven.substring(i, i + 1))) {
+                    key.put('a', seven.charAt(i));
+                    letterFrequencies.replace(seven.charAt(i), 0);
+                    break;
                 }
             }
 
+            for (Map.Entry<Character, Integer> pair : letterFrequencies.entrySet()) {
+
+                if (pair.getValue() == 6) {
+                    key.put('b', pair.getKey());
+                    pair.setValue(0);
+
+                    for (int i = 0; i < four.length(); i++) {
+                        if (!seven.contains(four.substring(i, i + 1)) && four.charAt(i) != pair.getKey()) {
+                            key.put('d', four.charAt(i));
+                            letterFrequencies.replace(four.charAt(i), 0);
+                            break;
+                        }
+                    }
+
+                } else if (pair.getValue() == 4) {
+                    key.put('e', pair.getKey());
+                    pair.setValue(0);
+                } else if (pair.getValue() == 9) {
+                    key.put('f', pair.getKey());
+                    pair.setValue(0);
+                } else if (pair.getValue() == 8) {
+                    key.put('c', pair.getKey());
+                    pair.setValue(0);
+                }
+
+            }
+
+            for (Map.Entry<Character, Integer> pair : letterFrequencies.entrySet()) {
+                if (pair.getValue() == 7) {
+                    key.put('g', pair.getKey());
+                    pair.setValue(0);
+                    break;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (String outputDigit : e.outputValue()) {
+
+                sb.append(evaluateDigit(outputDigit, key));
+
+            }
+            answer  += Integer.parseInt(sb.toString());
 
         }
 
+        return String.valueOf(answer);
 
+    }
+
+
+    private record NotebookEntry(String[] signalPatterns, String[] outputValue) {
     }
 }
