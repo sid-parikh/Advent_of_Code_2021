0fd1673e838df972064d0ce920fe349ff93ed772
Sid Parikh <parikhsid16@gmail.com>
2021-12-12 00:59:01 -0500

    day 12

---
+++ src/main/java/com/sidparikh/advent/Utils.java
@@ -0,0 +1,47 @@
+package com.sidparikh.advent;
+
+public class Utils {
+
+    public record Loc(int x, int y) {}
+
+    public void print2dArray(int[][] arr) {
+        for (int[] row : arr) {
+            for (int n : row) {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void print2dArray(long[][] arr) {
+        for (long[] row : arr) {
+            for (long n : row) {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void print2dArray(char[][] arr) {
+        for (char[] row : arr) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void print2dArray(Object[][] arr) {
+        for (Object[] row : arr) {
+            for (Object o : row) {
+                System.out.print(o);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+}
