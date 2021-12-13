5819932842167f84fcedf8432102d001f857e802
Sid Parikh <parikhsid16@gmail.com>
2021-12-13 18:08:31 -0500

    DAY 13 yay

--- src/main/java/com/sidparikh/advent/solutions/Day13.java
+++ src/main/java/com/sidparikh/advent/solutions/Day13.java
@@ -17,30 +17,31 @@ public class Day13 extends Solution {
     public String partOne() {
         boolean[][] dots = new boolean[1500][1500];
         int i = 0;
+        int maxX = 0;
+        int maxY = 0;
         for (; i < input.size(); i++) {
             String line = input.get(i);
             if (line.isEmpty()) {
                 break;
             }
             String[] coords = line.split(",");
-            dots[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])] = true;
+            int x = Integer.parseInt(coords[0]);
+            int y = Integer.parseInt(coords[1]);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+            dots[x][y] = true;
         }
+        boolean[][] newArr = new boolean[maxX + 1][maxY + 1];
+        for (int r = 0; r <= maxX; r++) {
+            for (int c = 0; c <= maxY; c++) {
+                newArr[r][c] = dots[r][c];
+            }
+        }
+        dots = newArr;
         String firstFold = input.get(i + 1);
         firstFold = firstFold.substring(11);
         boolean isX = firstFold.charAt(0) == 'x';
         int val = Integer.parseInt(firstFold.substring(2));
-
-        int maxX = dots.length - 1;
-        int maxY = dots[0].length - 1;
-
-//        if (isX) {
-//            for (i = 0; ((val + i < dots.length) && (val - i >= 0)); i++) {
-//                for (int j = 0; j < dots[val].length; j++) {
-//                    dots[val - i][j] |= dots[val + i][j];
-//                }
-//            }
-//            maxX = val - 1;
-//        }
 
         dots = foldX(dots, val);
 
@@ -52,96 +53,94 @@ public class Day13 extends Solution {
         }
 
         return String.valueOf(count);
-
     }
 
     private static boolean[][] foldX(boolean[][] dots, int val) {
-
+        final int L = dots[0].length;
         boolean[][] answer;
-        if (val > dots.length - val) {
-            answer = new boolean[val][dots[0].length];
-            for (int i = 1; ((val + i < dots.length) && (val - i >= 0)); i++) {
-                for (int j = 0; j < dots[0].length; j++) {
-                    answer[answer.length - i][j] = dots[val - i][j] || dots[val + i][j];
+        if (val > dots.length / 2 - 1) {
+            answer = new boolean[val][L];
+            for (int i = 1; val + i < dots.length; i++) {
+                for (int j = 0; j < L; j++) {
+                    answer[val - i][j] = dots[val - i][j] || dots[val + i][j];
                 }
             }
-            for (int i = 0; i < val; i++) {
-                for (int j = 0; j < dots[0].length; j++) {
+            for (int i = 0; i <= (2 * val) - dots.length; i++) {
+                for (int j = 0; j < L; j++) {
                     answer[i][j] = dots[i][j];
                 }
             }
-
-            return answer;
         } else {
-            answer = new boolean[dots.length - val][dots[0].length];
-
-            for (int i = 1; ((val + i < dots.length) && (val - i >= 0)); i++) {
-                for (int j = 0; j < dots[0].length; j++) {
-                    answer[answer.length - i][j] = dots[val - i][j] || dots[val + i][j];
+            answer = new boolean[dots.length - val - 1][L];
+            final int offset = dots.length - (2 * val) - 1;
+            for (int i = 1; val + i < dots.length; i++) {
+                for (int j = 0; j < L; j++) {
+                    answer[val - i + offset][j] = dots[val - i][j] || dots[val + i][j];
                 }
             }
-            for (int i = 0; i < answer.length - val - 1; i++) {
-                for (int j = 0; j < dots[0].length; j++) {
-                    answer[i][j] = dots[dots.length - 1 - i][j];
-                }
-            }
-
-            return answer;
         }
+        return answer;
     }
 
 
 
     private static boolean[][] foldY(boolean[][] dots, int val) {
+        final int L = dots.length;
         boolean[][] answer;
-        if (val > dots[0].length - val) {
-            answer = new boolean[dots.length][val];
-            for (int i = 0; i < dots.length; i++) {
-                for (int j = 1; ((val + j < dots[0].length) && (val - j >= 0)); j++) {
-                    answer[i][answer[0].length - j] = dots[i][val - j] || dots[i][val + j];
+        if (val > dots[0].length / 2 - 1) {
+            answer = new boolean[L][val];
+
+            for (int i = 0; i < L; i++) {
+                for (int j = 1; val + j < dots[0].length; j++) {
+                    answer[i][val - j] = dots[i][val - j] || dots[i][val + j];
                 }
             }
-            for (int i = 0; i < answer.length; i++) {
-                for (int j = 0; j < val; j++) {
+
+            for (int i = 0; i < L; i++) {
+                for (int j = 0; j <= (2 * val) - dots[0].length; j++) {
                     answer[i][j] = dots[i][j];
                 }
             }
-
-            return answer;
         } else {
-            answer = new boolean[dots.length][dots[0].length - val];
+            answer = new boolean[L][dots[0].length - val];
 
-            for (int i = 0; i < dots.length; i++) {
-                for (int j = 1; ((val + j < dots[0].length) && (val - j >= 0)); j++) {
-                    answer[i][answer[0].length - j] = dots[i][val - j] || dots[i][val + j];
+            final int offset = dots[0].length - (2 * val) - 1;
+
+            for (int i = 0; i < L; i++) {
+                for (int j = 1; j + val < dots[0].length; j++) {
+                    answer[i][val - j + offset] = dots[i][val - j] || dots[i][val + j];
                 }
             }
 
-            for (int i = 0; i < answer.length; i++) {
-                for (int j = 0; j < answer[0].length - val - 1; j++) {
-                    answer[i][j] = dots[i][dots[0].length - 1 - j];
-                }
-            }
-
-            return answer;
         }
+        return answer;
     }
 
     @Override
     public String partTwo() {
         boolean[][] dots = new boolean[1500][1500];
         int i = 0;
+        int maxX = 0;
+        int maxY = 0;
         for (; i < input.size(); i++) {
             String line = input.get(i);
             if (line.isEmpty()) {
                 break;
             }
             String[] coords = line.split(",");
-            dots[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])] = true;
+            int x = Integer.parseInt(coords[0]);
+            int y = Integer.parseInt(coords[1]);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+            dots[x][y] = true;
         }
-
-        int maxX = dots.length - 1;
-        int maxY = dots[0].length - 1;
+        boolean[][] newArr = new boolean[maxX + 1][maxY + 1];
+        for (int r = 0; r <= maxX; r++) {
+            for (int c = 0; c <= maxY; c++) {
+                newArr[r][c] = dots[r][c];
+            }
+        }
+        dots = newArr;
         for (i = i + 1; i < input.size(); i++) {
             String intstructions = input.get(i);
             intstructions = intstructions.substring(11);
@@ -164,9 +163,9 @@ public class Day13 extends Solution {
 //            }
 //        }
 
-        for (i = 0; i < dots.length; i++) {
-            for (int j = 0; j < dots[0].length; j++) {
-                System.out.print(dots[i][j] ? '#' : '.');
+        for (i = 0; i < dots[0].length; i++) {
+            for (int j = 0; j < dots.length; j++) {
+                System.out.print(dots[j][i] ? '#' : '.');
             }
             System.out.println();
         }
